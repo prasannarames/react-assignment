8cc2dd2c257d7e971ded7aef546555aa291674d5
prasannarames <prasannaramesh98@gmail.com>
2023-05-15 10:27:34 +0530

    Initial ups

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
-function Home() {
+export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>home</h1>
+    </div>
+  );
 }
-
-export default Home
