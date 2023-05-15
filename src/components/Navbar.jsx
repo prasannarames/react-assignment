772e5c6cdfc115ab6025301da5d92ded31225b7d
prasannarames <prasannaramesh98@gmail.com>
2023-05-15 21:26:57 +0530

    navbar and styles

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <>
+    <div className='navbar'>
+    <a className='navItemsL'>All problems</a>
+    <a className='navItemsR'>Signup</a>
+    <a className='navItemsR'>Login</a>
+    </div>
+
+    </>
+  )
+}
+
+export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <h1>home</h1>
+      <Navbar/>
+      
     </div>
   );
 }
